266b75f79aa1bf9e075dcc26bbcc444bcb09a901
Sphuccc <86742768+Sphuccc@users.noreply.github.com>
2021-07-01 02:29:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
-const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+const CONFIG = { 
+    introTitle: 'péo ui!',
+    introDesc: `tui iu péo nhiều lắm í,
+               Thưong péo lắm cơ,'
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    title:'nên là...'
+    desc: 'mình quay về với nhau nheee',
+    btnYes: 'đồng ý hay tay hai chân luôn',
+    btnNo: 'khum',
+    question:'mình sẽ cùng thấy đổi vì nhau nha><<',
+    btnReply: 'được chứ :<',
+    reply: 'tất nhiên ùi mong là chúng ta sẽ hạnh phúc bên nhau',
+    mess: 'tui cũng mong v iu péo',
+    messDesc: 'hôm nào set kèo chơi game với bé nha',
+    btnAccept: 'Okiiiii luônn ',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
